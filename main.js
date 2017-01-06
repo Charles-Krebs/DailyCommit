c6e70b68a312351a086dd45bd9171a0370f61013
Charles-Krebs <charleskrebs@amdg.rockhursths.edu>
2017-01-06 11:27:53 -0600

    init

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
+if(window.location.hostname === '127.0.0.1') {
+    window.location = 'http://localhost:1898' ;
+}
+
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail());
+    
+    $('.g-signin2').hide();
 }
 
-<a href="#" onclick="signOut();">Sign out</a>
-<script>
-  function signOut() {
+var auth2;
+
+function appStart(){
+    console.log('hello');
+}
+
+function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
       console.log('User signed out.');
     });
-  }
-</script>
+    
+    $('.g-signin2').show();
+}
